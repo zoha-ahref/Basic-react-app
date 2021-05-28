9e811dec1899011f19fea4d95dc49b49168aab15
Zoha Kaukab <56480034+zoha-ahref@users.noreply.github.com>
2021-05-27 22:39:14 -0400

    Basic react greeting app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 //Create a React app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 
+const today = new Date();
+const currentTime = today.getHours();
+const customStyle = {
+  color: ""
+};
+
+let greeting = "";
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {greeting}
+  </h1>,
+  document.getElementById("root")
+);
 //or "Good Afternoon" if between 12PM and 6PM.
 //or "Good evening" if between 6PM and midnight.
 //Apply the "heading" style in the styles.css
